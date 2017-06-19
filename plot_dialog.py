102e08ef96233c3932e80e372cb47a8cda7df153
Joey Ramone <lesliemayer@yahoo.com>
2017-06-19 15:06:42 -0500

    Feat : commenting out band_center_btn.clicked action

--- plot_dialog.py
+++ plot_dialog.py
@@ -47,8 +47,8 @@ class PlotDialog(cls_dialog, ui_dialog):
         # self.endpoint_slider_upper.valueChanged.connect(lambda i: self.endpoint_slider_upper_label.setText('{'
         #                                                                                                    '}'.format(self.wavelengths[i])))
 
-        # lrm - will get error if comment this out
-        self.band_center_btn.clicked.connect(self.band_center)
+        # lrm
+        # self.band_center_btn.clicked.connect(self.band_center)
 
         # lrm  - will get error if comment this out
         self.band_minima_btn.clicked.connect(self.band_minima)
